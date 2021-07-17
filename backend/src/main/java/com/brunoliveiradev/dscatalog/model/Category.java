ca95fd7d99392aabc4f56954ea20a8fc960c5f91
brunoliveiradev <bruno.henrique0liveir4@gmail.com>
2021-07-17 15:24:50 -0300

    Category.java and CategoryResource.java created.

---
+++ backend/src/main/java/com/brunoliveiradev/dscatalog/model/Category.java
@@ -0,0 +1,48 @@
+package com.brunoliveiradev.dscatalog.model;
+
+import java.io.Serializable;
+
+//@Entity
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Category)) return false;
+
+        Category category = (Category) obj;
+
+        return getId().equals(category.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getId().hashCode();
+    }
+}
