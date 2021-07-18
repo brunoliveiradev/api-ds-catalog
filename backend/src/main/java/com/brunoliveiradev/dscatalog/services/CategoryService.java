61b70f02e8e47398f7c8a91f33fe8ca3c0431529
brunoliveiradev <bruno.henrique0liveir4@gmail.com>
2021-07-18 13:02:45 -0300

    Database seeding.

--- backend/src/main/java/com/brunoliveiradev/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/brunoliveiradev/dscatalog/services/CategoryService.java
@@ -3,6 +3,7 @@ package com.brunoliveiradev.dscatalog.services;
 import com.brunoliveiradev.dscatalog.model.Category;
 import com.brunoliveiradev.dscatalog.repositories.CategoryRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,6 +16,7 @@ public class CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
         return categoryRepository.findAll();
     }
