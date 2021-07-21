bc500c46eea6230a8dc6d5c5b724fb5a6550900f
brunoliveiradev <bruno.henrique0liveir4@gmail.com>
2021-07-21 20:21:05 -0300

    ControllerAdvice and ExceptionHandler created for EntityNotFoundException.

---
+++ backend/src/main/java/com/brunoliveiradev/dscatalog/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.brunoliveiradev.dscatalog.controllers.exceptions;
+
+import com.brunoliveiradev.dscatalog.services.exceptions.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler  {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException exception, HttpServletRequest request) {
+        StandardError errorBody = new StandardError();
+        errorBody.setTimestamp(Instant.now());
+        errorBody.setStatus(HttpStatus.NOT_FOUND.value());
+        errorBody.setError("Resource not found");
+        errorBody.setMessage(exception.getMessage());
+        errorBody.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorBody);
+    }
+}
