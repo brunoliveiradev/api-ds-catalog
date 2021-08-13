d25c435051a3231f4082535f22b8b4b43335a96f
brunoliveiradev <bruno.henrique0liveir4@gmail.com>
2021-08-13 13:51:49 -0300

    DataIntegrityViolationException Handler created.

--- backend/src/main/java/com/brunoliveiradev/dscatalog/controllers/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/brunoliveiradev/dscatalog/controllers/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.brunoliveiradev.dscatalog.controllers.exceptions;
 
+import com.brunoliveiradev.dscatalog.services.exceptions.DataBaseException;
 import com.brunoliveiradev.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,13 +15,27 @@ public class ResourceExceptionHandler  {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException exception, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
         StandardError errorBody = new StandardError();
         errorBody.setTimestamp(Instant.now());
-        errorBody.setStatus(HttpStatus.NOT_FOUND.value());
+        errorBody.setStatus(status.value());
         errorBody.setError("Resource not found");
         errorBody.setMessage(exception.getMessage());
         errorBody.setPath(request.getRequestURI());
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorBody);
+        return ResponseEntity.status(status).body(errorBody);
+    }
+
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandardError> dataBase(DataBaseException exception, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError errorBody = new StandardError();
+        errorBody.setTimestamp(Instant.now());
+        errorBody.setStatus(status.value());
+        errorBody.setError("Database Integrity Violation Exception");
+        errorBody.setMessage(exception.getMessage());
+        errorBody.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(errorBody);
     }
 }
